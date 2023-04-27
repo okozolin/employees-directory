102b16cc4ebece51622868a98c7d79d1a550cdb2
Orit Kozolin <orit.kozolin@seebo.com>
2023-04-27 14:05:28 +0300

    add route for username to be used in Employee details

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import {fetchEmployees} from "./services/api";
 import styled from "styled-components";
 import { MdFace } from "react-icons/md";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import EmployeeDetails from './components/EmployeeDetails'
 
 import { platformColors } from './constants/colors'
 import EmployeeListItem from "./components/EmployeeListItem";
@@ -21,19 +23,29 @@ function App() {
     }, []);
 
   return (
-    <div>
-      <MyLogo>
-        <MdFace color={platformColors.lightPink}/>
-        oritkozolin 2023
-      </MyLogo>
-      <h1>Employees</h1>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/:username" >
+            {({ match }) => <EmployeeDetails username={match.params.username} />}
+          </Route>
+          {/*<Route path="/:username" element={<Home />} />*/}
+        </Routes>
+      </Router>
+      <div>
+        <MyLogo>
+          <MdFace color={platformColors.lightPink}/>
+          oritkozolin 2023
+        </MyLogo>
+        <h1>Employees</h1>
 
-      {employees.map((employee)=>(
-        <div>
-          <EmployeeListItem employee={employee} employeesById={employeesById}/>
-        </div>
-      ))}
-  </div>
+        {employees.map((employee)=>(
+          <div>
+            <EmployeeListItem employee={employee} employeesById={employeesById}/>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
 
---
+++ src/components/EmployeeDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function EmployeeDetails(props) {
+  return (
+    <div>
+      Details of employee
+    </div>
+  );
+}
+
+export default EmployeeDetails;
